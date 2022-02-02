454f2939d4d8cbd7c34725ae224ec764bf37f104
WahidinAji <a17wahidin@gmail.com>
2022-02-02 23:15:01 +0700

    add pk to users table, and make text data type on password users. Reviewed by Reinaldy

--- schema/user.sql
+++ schema/user.sql
@@ -1,17 +1,17 @@
 create TYPE user_role AS ENUM ('admin','customer');
 CREATE TABLE users(
-    id uuid not null,
+    id uuid not null primary key,
     name varchar(100) not null,
-    password varchar(100) not null,
+    password text not null,
     role user_role default 'customer',
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
 insert into users (id, name, password,role) values
-('882c4b8e-6c72-4f32-ac19-2f1431c17c0e','admin','admin-secret','admin'),
-('f0ed9709-c6ef-48b2-8beb-0f88b60d6c83','aji','aji-secret','customer'),
-('4d1d51fe-5a6c-4eb7-92a7-38ed97252881','tia','tia-secret','customer');
+('882c4b8e-6c72-4f32-ac19-2f1431c17c0e','admin','$2a$10$QT03WTl5/6gkhuVPodBD6ebJ.y54PsxJMQWekvjtzRio0Pv95oOty','admin'),
+('f0ed9709-c6ef-48b2-8beb-0f88b60d6c83','aji','$2a$10$vuA/29AVCphLnyL4cQYfoeya8dEGuFPjQNHk/XsNL2D8T4CbvCYbe','customer'),
+('4d1d51fe-5a6c-4eb7-92a7-38ed97252881','tia','$2a$10$AN2knlxbSmJNOfNPGXAQSuOXCdFIR8rTHFYJvKWoA1UjRLjNgW1tW','customer');
 
 select * from users;
 -- drop table users;
